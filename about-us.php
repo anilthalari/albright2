40ac07e8c62d3714b62d383afa2459dffedaa9f5
anilkumar <33172168+anilthalari@users.noreply.github.com>
2019-06-10 16:49:29 +0530

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Albright Technologies</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/service.css">
+
+  <div class="header-top">
+    <!-- HEADER -->
+    <nav class="navbar navbar-default" role="navigation" id="header">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">
+            <!-- <img src="img/vijay_hospital_logo_1.png" alt="vijay_hospital_logo_1" class="img-responsive"> -->
+            <h2><span>Albright</span></h2>
+            <p>Technologies</p>
+          </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+               <ul class="nav navbar-nav navbar-right">
+              <li><a href="/">Home</a></li>
+			  <li class="active"><a href="about-us.php">About Us</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                  aria-expanded="false">Courses</a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">CompTIA - A+</a></li>
+                  <li><a href="#">Network+</a></li>
+                  <li><a href="#">CCNA, CCNP(Routing & Switching)</a></li>
+                  <li><a href="#">Microsoft (MCSA - 2012)</a></li>
+				  <li><a href="#">Linux - (RHCE, RHCSA)</a></li>
+                  <li><a href="#">Cloud Computing</a></li>
+				  <li><a href="#">Big Data</a></li>
+				  <li><a href="#">Hadoop</a></li>
+				   <li><a href="#">Web Designing</a></li>
+				    <li><a href="#">Web Developing</a></li>
+                </ul>
+              </li>
+              <li><a href="gallery.php">Gallery</a></li>
+              <li><a href="contact.php">Contact Us</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+      </div>
+    </nav>
+  </div>
+</head>
+
+<body>
+
+  <div class="gallery-albums">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="heading-block text-center">
+            <h2 class="heading">About Us</h2>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="album-container">
+      
+       
+     <div class=" col-sm-12">
+              <div class="heading-block text-left">
+                  <h3 class="sub-heading text-center" style="color:black;font-family:cursive;">Albright-Introduction</h3>
+      <!--- <p class="sub-heading">Albright</p>--->
+	  <div class=" col-sm-1"></div>
+	  <div class=" col-sm-10">
+				  <p>Albright Technologies is amongst Asia&#39;s top IT training organizations, headquartered in hyderabad,
+India. The company was incorporated in 2005, with a vision to achieve its goals through its firm belief in
+the doctrine of improvement at every stage. The people at Albright are deeply committed to learning
+and improving while dealing with specific needs or divergent applications as the occasion demands.</p>
+<p>Albright is a premier training organization with focus on IT training and offers its services to individuals, Corporate and education institutions across the country, offering high quality and cost-effective professional training.</p>
+<p>Albright Domain Expertise</p>
+<p>Over 25 certified trainers on cutting edge technologies</p>
+<p>Delivered 350 million man hours of training</p>
+<p>Delivered over 1,00,000 man hours of corporate training</p>
+<p>Conducted 10,000 hours of TTT program (Train the Trainer)</p>
+<p>Albright’s business has grown at 40% CAGR year after year. During this phase, Albright had been successful in carving its goodwill in the industry. As a result, some of the renowned Fortune 100 organizations rely on Albright for catering to their manpower requirements. </p>
+<p>The goodwill is not only because Albright provides skilled manpower in the areas of hardware, networking, database management, security and storage management, but also because the students are taught the latest technology advancements in the industry. This is coupled with hands-on lab sessions where in the students are faced with simulations of live situations that may arise on a day to day level in organizations. This equips the students to proactively act to situations and implement the best possible solution befitting the situation.</p>
+<p>Albright has gives the training with <b> Microsoft, Vmware, Cisco, Citrix, Amazone Web Service,   CompTIA, Red hat, etc.. </b> and others and provides knowledge the latest technological advancements of its partners. This has resulted in a substantial increase in the student base for Albright. Albright's students include those studying 10th or 12 / Degree / Btech / Mtech / Msc / MCA Standard or are pursuing  / completed graduation, as well as corporates. </p>				  
+<p>Albright has been in the IMS industry for the last 13 years and has made a strong goodwill in the market. A part of the goodwill goes to the fact that Albright has been imparting industry relevant and up-to-date training to its students and thereby preparing them to become an invaluable addition to any company's workforce.</p>
+<p>The remaining part of the goodwill goes to its valued partners who have been associated with Albright. These partners have collaborated with Albright and have opened doors for students to get acquainted with the latest developments in the technology domains pertaining to their organizations.</p>				  
+<p><b>Albright owns a unique Virtual cloud data centre and provides ‘Learning-as-a-Service’ training on the cloud. It will eliminate the need for large and complex infrastructure, making learning simple and affordable and enable participants to get an easy access to learn, manage and build cloud anywhere, anytime with any kind of computing device.</b></p>
+<h3 class="sub-heading" style="color:black;font-family:cursive;">Placements - Career Services</b></h3>
+<p>Albright  has three tier architecture for placements; Central Placement Cell (CPC), Regional Placement Cell (RPC) & Local Placement Cell (LPC). Every Albright Center has a Placement cell and all the eligible student would be assisted with the Placement opportunity to the desired Company. </p>
+<p>Student can apply directly to the company or the placement channel, which is Central Placement Cell (CPC), Regional Placement Cell (RPC). </p>
+<p>The consulting division ensures through the said architecture a placement in IT industry to every successful pass out from Albright.</p>
+<h3 class="sub-heading" style="color:black;font-family:cursive;">Courses</b></h3>
+<p><b>Individuals: Albright</b> hosts a wide range of courses for individual students at its centers across the country through online or offline classes. They include career courses and modular courses which lead to industry recognized international certifications.</p>
+<p>Career Courses: Agnetpro, Cloud Computing…</p>
+<p><b>ITMS, Networking, Dot Net, Java, Android App Development, Web App Development, Digital Enterprise Server Management, Big Data and Hadoop, Cloud Computing and other modular job-oriented courses.</b></p>
+<p>Modular Courses:  Microsoft Technologies: MCSA Windows 2012 Server Administration, ,Vmware Esx/Esxi Server Virtualization, Citrix Xen App & Desktop, Microsoft Hyper-V, MCITP, Cisco Technologies: CCNP, CCNA, Red Hat: RHCE, Security: CCSA, Ethical Hacking,  CompTIA: A+, Network +, Etc.,</p>
+<p>
+<b>Institutions: </b>The UQ initiative has seen Albright partner with colleges and universities to provide parallel job focused courses to students
+</p>
+<p>U.Q, an Albright initiative, is primarily a skill school with the objective to build a convergence between the conventional university education & the dynamic and global skill set required by the industry. UQ proposes to offer customized training on cutting edge technologies to the students within the college premises and alongside their college curriculum. Enabling the beneficiaries get the best of both college & technical training, thereby ensuring students graduating from our academic institutions are productive from the first hour of the very first day they join their respective organizations.</p>
+</div>
+			  <div class=" col-sm-1"></div>
+            </div>
+       
+    
+     
+       
+    
+
+
+      
+
+      
+
+       
+
+       
+
+     
+			
+			
+			
+			
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    <!-- Footer -->
+    <footer class="footer-main">
+      <!-- Container -->
+      <div class="container-fluid">
+        <!-- Contact Detail -->
+        <div class="row">
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <aside class="widget widget-about">
+              <!-- <img src="images/" alt="LOGO" class="img-responsive wow slideInUp"> -->
+              <h1 class="wow slideInUp">ALBRIGHT TECHNOLOGIES</h1><br>
+              <p class=" wow slideInUp">albright technologies, experts in IT Training and placement Services in
+                Vijayawada, Andhra Pradesh.</p>
+              <h3 class="widget-title wow slideInUp">Working Hours</h3>
+              <div class="time-schedule">
+                <p class="wow slideInUp">Monday - Friday <span>10.00 AM - 06:30 PM</span></p>
+                <p class="wow slideInUp">Saturday <span>10.00 AM - 04:30 PM</span></p>
+              </div>
+            </aside>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12">
+           
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <aside class="widget address wow slideInUp">
+              <h3 class="widget-title">Contact Us</h3>
+              <p class="wow slideInUp">1st floor CHNR complex,</p>
+              <p class="wow slideInUp">Besant Road, near LIC building</p>
+              <p class="wow slideInUp">Landmark(opposite raghavaayya park)</p>
+              <p class="wow slideInUp">Vijayawada - 2</p>
+              <p class="wow slideInUp"> (+91) 9032493444</p>
+              <p class="wow slideInUp"> 0866-6630303</p>
+            </aside>
+          </div>
+        </div>
+      </div><!-- Container /- -->
+      <!-- Bottom Footer -->
+
+      <div class="bottom-footer wow slideInUp">
+        <div class="container-fluid">
+          <p class="powered">Powered By <a href="#" target="_blank"></a></p>
+          <p>Albright &copy; <span class="c-year"> </span> All Rights Reserved.</p>
+        </div><!-- Bottom Footer /- -->
+      </div>
+    </footer>
+
+    <a href="#intro" class="btn-go-top"><span class="ion-ios-arrow-up"></span></a>
+  </div>
+  </div>
+  <script src="js/services.js"></script>
+  <script src="js/app.js"></script>
+</body>
+
+</html>
---
+++ contact.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Albright Technologies</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/service.css">
+
+  <div class="header-top">
+    <!-- HEADER -->
+    <nav class="navbar navbar-default" role="navigation" id="header">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">
+            <!-- <img src="img/vijay_hospital_logo_1.png" alt="vijay_hospital_logo_1" class="img-responsive"> -->
+            <h2><span>Albright</span></h2>
+            <p>Technologies</p>
+          </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+               <ul class="nav navbar-nav navbar-right">
+              <li><a href="/">Home</a></li>
+			  <li><a href="about-us.php">About Us</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                  aria-expanded="false">Courses</a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">CompTIA - A+</a></li>
+                  <li><a href="#">Network+</a></li>
+                  <li><a href="#">CCNA, CCNP(Routing & Switching)</a></li>
+                  <li><a href="#">Microsoft (MCSA - 2012)</a></li>
+				  <li><a href="#">Linux - (RHCE, RHCSA)</a></li>
+                  <li><a href="#">Cloud Computing</a></li>
+				  <li><a href="#">Big Data</a></li>
+				  <li><a href="#">Hadoop</a></li>
+				   <li><a href="#">Web Designing</a></li>
+				    <li><a href="#">Web Developing</a></li>
+                </ul>
+              </li>
+              <li><a href="gallery.php">Gallery</a></li>
+              <li class="active"><a href="contact.php">Contact Us</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+      </div>
+    </nav>
+  </div>
+</head>
+
+<body>
+
+  
+    <div class="container-fluid">
+    <br><br>
+	  
+      <div class="row">
+        <div class="col-sm-12">
+        
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3825.344849165291!2d80.62410185028423!3d16.508679088554793!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a35f003c51410ef%3A0x56a1aa01ee406888!2sCHNR+Complex%2C+Besant+Rd%2C+Governor+Peta%2C+Vijayawada%2C+Andhra+Pradesh+520002!5e0!3m2!1sen!2sin!4v1559123235879!5m2!1sen!2sin" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+       <br><br>
+    
+       
+       </div>
+    </div>
+  
+    <!-- Footer -->
+    <footer class="footer-main">
+      <!-- Container -->
+      <div class="container-fluid">
+        <!-- Contact Detail -->
+        <div class="row">
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <aside class="widget widget-about">
+              <!-- <img src="images/" alt="LOGO" class="img-responsive wow slideInUp"> -->
+              <h1 class="wow slideInUp">ALBRIGHT TECHNOLOGIES</h1><br>
+              <p class=" wow slideInUp">albright technologies, experts in IT Training and placement Services in
+                Vijayawada, Andhra Pradesh.</p>
+              <h3 class="widget-title wow slideInUp">Working Hours</h3>
+              <div class="time-schedule">
+                <p class="wow slideInUp">Monday - Friday <span>10.00 AM - 06:30 PM</span></p>
+                <p class="wow slideInUp">Saturday <span>10.00 AM - 04:30 PM</span></p>
+              </div>
+            </aside>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12">
+          >
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <aside class="widget address wow slideInUp">
+              <h3 class="widget-title">Contact Us</h3>
+              <p class="wow slideInUp">1st floor CHNR complex,</p>
+              <p class="wow slideInUp">Besant Road, near LIC building</p>
+              <p class="wow slideInUp">Landmark(opposite raghavaayya park)</p>
+              <p class="wow slideInUp">Vijayawada - 2</p>
+              <p class="wow slideInUp"> (+91) 9032493444</p>
+              <p class="wow slideInUp"> 0866-6630303</p>
+            </aside>
+          </div>
+        </div>
+      </div><!-- Container /- -->
+      <!-- Bottom Footer -->
+
+      <div class="bottom-footer wow slideInUp">
+        <div class="container-fluid">
+          <p class="powered">Powered By <a href="#" target="_blank"></a></p>
+          <p>Albright &copy; <span class="c-year"> </span> All Rights Reserved.</p>
+        </div><!-- Bottom Footer /- -->
+      </div>
+    </footer>
+
+    <a href="#intro" class="btn-go-top"><span class="ion-ios-arrow-up"></span></a>
+  </div>
+  </div>
+  <script src="js/services.js"></script>
+  <script src="js/app.js"></script>
+</body>
+
+</html>
